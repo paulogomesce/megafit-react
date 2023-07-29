8e41001024e7acd880dd903345a42ab8bc005bba
paulo.gomes <dbc.pgomes@unimedpoa.com.br>
2023-07-29 11:37:43 -0300

    Ajustes no formulário de cadastro.

--- src/pages/Cadastro/Academia.tsx
+++ src/pages/Cadastro/Academia.tsx
@@ -32,7 +32,7 @@ export default function Academia() {
                     </div>
                 </div>
 
-                <div className="mb-4.5 flex flex-col gap-6 xl:flex-row">
+                <div className="mb-4 flex flex-col gap-6 xl:flex-row">
                     {/* CPF */}
                     <div className="w-full xl:w-1/2">
                         <Label texto="CPF" />
